483bef12ac58e9bf6ef45aefc099502cca856879
Julia Gahovich <julia.gahovich@easybrain.com>
2021-03-15 19:47:24 +0300

    fix first test

--- tests/db_tests/test_new_user_add_to_group.py
+++ tests/db_tests/test_new_user_add_to_group.py
@@ -1,10 +1,8 @@
 from pages.django_pages.base_page import BasePage
-from pages.django_pages.main_page import MainPage
 from time import sleep
 
 
 def test_open_admin(browser):
     base_page = BasePage(browser)
     base_page.open_base_page()
-    print('ok')
     sleep(5)
